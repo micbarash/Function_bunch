fbdfd8e9e198e869825c4993772c7acd81a6ee37
unknown <micbarash@gmail.com>
2017-12-28 12:36:51 +0200

    Changed layout of tempConverter (added form input).

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// *** Reverse String ***
+let reverseString = function() {
+
+};
 // *** Remove From Array ***
 let removeFromArray = function(a, b) {
   a = prompt("Enter first array");
@@ -67,14 +71,14 @@ let leapYear = function(year) {
 
 // *** Temperature Converter ***
 let ctof = function(tempc) {
-  tempc = prompt('Enter Temperature in (C) to be Converted');
-  let farH = tempc * 1.8 + 32;
+  tempc = document.getElementById('frm1');
+  let farH = tempc.value * 1.8 + 32;
   let farhRound = Math.round(farH * 10) / 10;
-  alert(tempc + "C" + " in Fahrenheit is " + farhRound);
+  alert(tempc.value + "C" + " in Fahrenheit is " + farhRound);
 }
 let ftoc = function(tempf) {
-  tempf = prompt('Enter Temperature in (F) to be Converted')
-  let celS = (tempf - 32) / 1.8;
+  tempf = document.getElementById('frm2');
+  let celS = (tempf.value - 32) / 1.8;
   let celsRound = Math.round(celS * 10) / 10;
-  alert(tempf + "F" + " in Celsius is " + celsRound);
+  alert(tempf.value + "F" + " in Celsius is " + celsRound);
 }
